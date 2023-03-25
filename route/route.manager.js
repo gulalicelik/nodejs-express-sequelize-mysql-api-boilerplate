1542accae80c9fc2baa24f357e3be7a5bbaf1ae7
gulalicelik <gulalicelikmi@gmail.com>
2023-03-25 21:47:16 +0300

    Add Swagger documentation to API endpoints with swagger-jsdoc and swagger-ui-express packages

--- route/route.manager.js
+++ route/route.manager.js
@@ -1,5 +1,8 @@
 const userRoute = require('./user.route');
 const helloRoute = require('./hello.route');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.js');
+
 
 const routeManager = (app) => {
     app.use('/', helloRoute);
