7cf0d986a6f8e1586e177134e54f6fdb0a931315
gulalicelik <gulalicelikmi@gmail.com>
2023-03-28 05:34:24 +0300

    Update package.json version to 1.0.5 and add new installation script for creating a Node.js API boilerplate

--- bin/cli.js
+++ bin/cli.js
@@ -1,30 +1,94 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process');
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
 
-const runCommand = command => {
+// Utility functions
+const exec = util.promisify(require('child_process').exec);
+async function runCmd(command) {
     try {
-        execSync(`${command}`, { stdio: 'inherit' });
-    } catch (e) {
-        console.error('Failed to execute ${command}', e);
-        return false;
+        const { stdout, stderr } = await exec(command);
+        console.log(stdout);
+        console.log(stderr);
+    } catch {
+        (error) => {
+            console.log(error);
+        };
     }
-    return true;
-};
-
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/gulalicelik/nodejs-express-sequelize-mysql-api-boilerplate.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
-
-console.log(`Cloning the repository with name ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) process.exit(-1);
-
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+}
 
 
-console.log(
-    'Congratulations! You are ready. Follow the following commands to start'
-);
-console.log(`cd ${repoName} && npm start:dev`);
+// Validate arguments
+if (process.argv.length < 3) {
+    console.log('Please specify the target project directory.');
+    console.log('For example:');
+    console.log('    npx create-nodejs-app my-app');
+    console.log('    OR');
+    console.log('    npm init nodejs-app my-app');
+    process.exit(1);
+}
+
+// Define constants
+const ownPath = process.cwd();
+const folderName = process.argv[2];
+const appPath = path.join(ownPath, folderName);
+const repo = 'https://github.com/gulalicelik/nodejs-express-sequelize-mysql-api-boilerplate.git';
+
+// Check if directory already exists
+try {
+    fs.mkdirSync(appPath);
+} catch (err) {
+    if (err.code === 'EEXIST') {
+        console.log('Directory already exists. Please choose another name for the project.');
+    } else {
+        console.log(err);
+    }
+    process.exit(1);
+}
+
+async function setup() {
+    try {
+        // Clone repo
+        console.log(`Downloading files from repo ${repo}`);
+        await runCmd(`git clone --depth 1 ${repo} ${folderName}`);
+        console.log('Cloned successfully.');
+        console.log('');
+
+        // Change directory
+        process.chdir(appPath);
+
+        // Install dependencies
+
+        console.log('Installing dependencies...');
+
+        await runCmd('npm install');
+
+        console.log('Dependencies installed successfully.');
+        console.log();
+
+
+        // Delete .git folder
+        await runCmd('npx rimraf ./.git');
+
+        // Remove extra files
+        fs.unlinkSync(path.join(appPath, 'bin', 'cli.js'));
+        fs.rmdirSync(path.join(appPath, 'bin'));
+
+
+        console.log('Installation is now complete!');
+        console.log();
+
+        console.log('We suggest that you start by typing:');
+        console.log(`    cd ${folderName}`);
+        console.log('    npm run start:dev');
+        console.log('    pm2 list');
+        console.log('    pm2 logs');
+        console.log();
+        console.log('Enjoy your Node.js API boilerplate!');
+        console.log('Check README.md for more info.');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+setup();
